01e0d17be8c433cb56d8cb1c205bd7a665db86a7
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-26 12:37:06 +0000

    feat: add check for arrayType

--- R/readManifest.r
+++ R/readManifest.r
@@ -3,7 +3,6 @@
 #' Reads in a manifest file that has a very specific file name depending on
 #' the given array type. Important columns are standardised by name and contents
 #'
-#' @param arrayType This is a string with a value from c("450K", "V1", "V2")
 #' @param referenceDirectory This is a string pointing to the directory in
 #'  which the manifest files exist in. It is expected to contain:
 #'  "450K_reference/AllProbeIlluminaAnno.Rdata"
@@ -12,13 +11,15 @@
 #' @param probeMatchingIndex This is a vector of probe IDs that are present
 #'  in your dataset. This could be obtained via extracting the rownames of your
 #'  raw beta matrix, or from a gds file that contains probe IDs.
+#' @param arrayType This is a string with a value from c("450K", "V1", "V2")
 #'
 #' @return A data frame with varying columns. Certain columns are standardised:
 #'  designType - The infinium design type
 #'  CHR - The chromosome number in the form 'chrxyz'
-readManifest <- function(arrayType,
-                         referenceDirectory,
-                         probeMatchingIndex) {
+readManifest <- function(referenceDirectory,
+                         probeMatchingIndex,
+                         arrayType = c("450K", "V1", "V2")) {
+  arrayType <- match.arg(arrayType)
   if (arrayType == "450K") {
     # Loads probeAnnot (needs generalising in the future)
     load(file.path(
