bb7b9fe13e2d8a50401286b4bf0a67f2dcd85a73
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-26 12:30:36 +0000

    docs: add roxygen comments

--- R/readManifest.r
+++ R/readManifest.r
@@ -1,3 +1,21 @@
+#' Read and Standardise a Manifest File
+#'
+#' Reads in a manifest file that has a very specific file name depending on
+#' the given array type. Important columns are standardised by name and contents
+#'
+#' @param arrayType This is a string with a value from c("450K", "V1", "V2")
+#' @param referenceDirectory This is a string pointing to the directory in
+#'  which the manifest files exist in. It is expected to contain:
+#'  "450K_reference/AllProbeIlluminaAnno.Rdata"
+#'  "EPICArray/EPIC.anno.CRCh38.tsv"
+#'  "EPICArray/EPIC-8v2-0_A1.csv"
+#' @param probeMatchingIndex This is a vector of probe IDs that are present
+#'  in your dataset. This could be obtained via extracting the rownames of your
+#'  raw beta matrix, or from a gds file that contains probe IDs.
+#'
+#' @return A data frame with varying columns. Certain columns are standardised:
+#'  designType - The infinium design type
+#'  CHR - The chromosome number in the form 'chrxyz'
 readManifest <- function(arrayType,
                          referenceDirectory,
                          probeMatchingIndex) {
